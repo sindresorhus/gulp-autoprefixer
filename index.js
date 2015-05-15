834f7ea9ee5e2b994c33f020773f78e423d5e48f
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-16 00:00:22 +0200

    simplify error handling and improve warnings output
    closes #32

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 var gutil = require('gulp-util');
 var through = require('through2');
-var autoprefixer = require('autoprefixer-core');
 var applySourceMap = require('vinyl-sourcemaps-apply');
 var objectAssign = require('object-assign');
+var autoprefixer = require('autoprefixer-core');
 var postcss = require('postcss');
-var CssSyntaxError = require('postcss/lib/css-syntax-error');
 
 module.exports = function (opts) {
 	return through.obj(function (file, enc, cb) {
@@ -19,33 +18,31 @@ module.exports = function (opts) {
 			return;
 		}
 
-		try {
-			var fileOpts = objectAssign({}, opts);
-			var processor = postcss()
-				.use(autoprefixer(fileOpts))
-				.process(file.contents.toString(), {
-					map: file.sourceMap ? {annotation: false} : false,
-					from: file.path,
-					to: file.path
-				});
-
-			processor.then(function (res) {
-				file.contents = new Buffer(res.css);
-
-				if (res.map && file.sourceMap) {
-					applySourceMap(file, res.map.toString());
-				}
-
-				if (res.warnings && res.warnings()) {
-					res.warnings().forEach(function (warn) {
-						gutil.log(warn.toString());
-					});
-				}
-
-				cb(null, file);
+		var fileOpts = objectAssign({}, opts);
+		var processor = postcss()
+			.use(autoprefixer(fileOpts))
+			.process(file.contents.toString(), {
+				map: file.sourceMap ? {annotation: false} : false,
+				from: file.path,
+				to: file.path
 			});
-		} catch (err) {
-			var cssError = err instanceof CssSyntaxError;
+
+		processor.then(function (res) {
+			file.contents = new Buffer(res.css);
+
+			if (res.map && file.sourceMap) {
+				applySourceMap(file, res.map.toString());
+			}
+
+			var warnings = res.warnings();
+
+			if (warnings.length > 0) {
+				gutil.log('gulp-autoprefixer:', '\n  ' + warnings.join('\n  '));
+			}
+
+			cb(null, file);
+		}).catch(function (err) {
+			var cssError = err.name === 'CssSyntaxError';
 
 			if (cssError) {
 				err.message = err.message + err.showSourceCode();
@@ -55,6 +52,6 @@ module.exports = function (opts) {
 				fileName: file.path,
 				showStack: !cssError
 			}));
-		}
+		});
 	});
 };
