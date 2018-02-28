e954e6e8044fcfe2dcd54d1d40333aa681b84808
johanblumenberg <johan.blumenberg@gmail.com>
2018-02-28 10:27:12 +0100

    Update sourcemap paths to be relative to `file.base` (#92)

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ module.exports = opts => {
 			file.contents = Buffer.from(res.css);
 
 			if (res.map && file.sourceMap) {
-				applySourceMap(file, res.map.toString());
+				const map = res.map.toJSON();
+				map.file = file.relative;
+				map.sources = map.sources.map(() => file.relative);
+				applySourceMap(file, map);
 			}
 
 			const warnings = res.warnings();
